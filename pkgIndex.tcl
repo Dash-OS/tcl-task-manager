789c7b82dcd1c653d3746cdfd213040cf140fe8c
bradynapier <brady.napier@dashos.net>
2017-03-19 02:29:13 +0000

    Updates

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,9 +1,9 @@
 package ifneeded task 1.0 [list apply {{dir} {
-	uplevel #0 [ list source [file join $dir task.tcl] ]
+	uplevel #0 [ list source [file join $dir task-1.0.tm] ]
 	package provide task 1.0
 }} $dir]
 
 package ifneeded every 1.0 [list apply {{dir} {
-	uplevel #0 [ list source [file join $dir every.tcl] ]
+	uplevel #0 [ list source [file join $dir every-1.0.tm] ]
 	package provide every 1.0
 }} $dir]
